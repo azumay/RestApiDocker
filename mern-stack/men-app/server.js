500ca277beaa68d2ef3b118abc919d2531660c02
Xavi Yamuza <xavi_ym@hotmail.com>
2022-05-08 18:49:00 +0200

    add New POST remove users

--- mern-stack/men-app/server.js
+++ mern-stack/men-app/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-const {loginUser, registerUser, colaUsers, getCola } = require("./controllers/user.controller");
+const {loginUser, registerUser, colaUsers, getCola, removeUser } = require("./controllers/user.controller");
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +15,8 @@ app.post('/users/register', registerUser);
 app.post('/users/cola', colaUsers);
 
 app.get('/users/getcola', getCola);
+
+app.post('/users/remove', removeUser);
 
 app.listen(8080, () => {
     console.log('Server is runing on port 8080');
